88ab5c635120d1cc5287008117dcf3bb67a392f5
natalia-franca-reis <89528948+natreis02@users.noreply.github.com>
2023-11-26 13:29:25 -0300

    Final version

--- project_2.py
+++ project_2.py
@@ -10,7 +10,6 @@ from tensorflow.keras.layers import RepeatVector, Dense, Activation
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Model
-from tensorflow.keras.activations import softmax
 import tensorflow.keras.backend as K
 import tensorflow as tf
 import numpy as np
@@ -21,6 +20,26 @@ import random
 from tqdm import tqdm
 from babel.dates import format_date
 import matplotlib.pyplot as plt
+
+def softmax(x, axis=1):
+    """Softmax activation function.
+    # Arguments
+        x : Tensor.
+        axis: Integer, axis along which the softmax normalization is applied.
+    # Returns
+        Tensor, output of softmax transformation.
+    # Raises
+        ValueError: In case `dim(x) == 1`.
+    """
+    ndim = K.ndim(x)
+    if ndim == 2:
+        return K.softmax(x)
+    elif ndim > 2:
+        e = K.exp(x - K.max(x, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
 
 def string_to_int(string, length, vocab):
     """
@@ -79,7 +98,7 @@ file_paths_v = {
 }
 
 # Load the data from the files in a single line using dictionary comprehension
-data = {name: pickle.load(open(path, 'rb')) for name, path in file_paths_v.items()}
+data = {name: pickle.load(open(path, 'rb')) for name, path in file_paths_n.items()}
 
 dataset = data['dataset']
 human_vocab = data['human_vocab']
@@ -98,7 +117,7 @@ repeator = RepeatVector(Tx)
 concatenator = Concatenate(axis = -1)
 densor1 = Dense(10, activation = "tanh")
 densor2 = Dense(1, activation = "relu")
-activator = Activation('softmax', name = "attention_weights")
+activator = Activation(softmax, name = "attention_weights")
 dotor = Dot(axes = 1)
 
 def one_step_attention(a, s_prev):
@@ -275,45 +294,81 @@ def int_to_string(ints, inv_vocab):
     l = [inv_vocab[i] for i in ints]
     return l
 
-def plot_attention_map(model, input_vocabulary, inv_output_vocabulary, text, n_s = 64, Tx = 30, Ty = 10):
+def plot_attention_map(modelx, input_vocabulary, inv_output_vocabulary, text, n_s = 128, num = 7):
     """
     Plot the attention map.
-
+  
     """
-    attention_map = np.zeros((Ty, Tx))
+    attention_map = np.zeros((10, 30))
+    layer = modelx.get_layer('attention_weights')
+
+    Ty, Tx = attention_map.shape
+    
+    human_vocab_size = 37
+    
+    # Well, this is cumbersome but this version of tensorflow-keras has a bug that affects the 
+    # reuse of layers in a model with the functional API. 
+    # So, I have to recreate the model based on the functional 
+    # components and connect then one by one.
+    # ideally it can be done simply like this:
+    # layer = modelx.layers[num]
+    # f = Model(modelx.inputs, [layer.get_output_at(t) for t in range(Ty)])
+    #
+    
+    X = modelx.inputs[0] 
+    s0 = modelx.inputs[1] 
+    c0 = modelx.inputs[2] 
+    s = s0
+    c = s0
+    
+    a = modelx.layers[2](X)  
+    outputs = []
+
+    for t in range(Ty):
+        s_prev = s
+        s_prev = modelx.layers[3](s_prev)
+        concat = modelx.layers[4]([a, s_prev]) 
+        e = modelx.layers[5](concat) 
+        energies = modelx.layers[6](e) 
+        alphas = modelx.layers[7](energies) 
+        context = modelx.layers[8]([alphas, a])
+        # Don't forget to pass: initial_state = [hidden state, cell state] (≈ 1 line)
+        s, _, c = modelx.layers[10](context, initial_state = [s, c]) 
+        outputs.append(energies)
+
+    f = Model(inputs=[X, s0, c0], outputs = outputs)
+    
 
     s0 = np.zeros((1, n_s))
     c0 = np.zeros((1, n_s))
-    layer = model.layers[7]
-
-    encoded = np.array(string_to_int(text, Tx, input_vocabulary)).reshape((1, Tx))
+    encoded = np.array(string_to_int(text, Tx, input_vocabulary)).reshape((1, 30))
     encoded = np.array(list(map(lambda x: to_categorical(x, num_classes=len(input_vocabulary)), encoded)))
 
-    f = K.function(model.inputs, [layer.get_output_at(t) for t in range(Ty)])
+    
     r = f([encoded, s0, c0])
-
+        
     for t in range(Ty):
         for t_prime in range(Tx):
-            attention_map[t][t_prime] = r[t][0,t_prime,0]
+            attention_map[t][t_prime] = r[t][0, t_prime]
 
     # Normalize attention map
     row_max = attention_map.max(axis=1)
     attention_map = attention_map / row_max[:, None]
 
-    prediction = model.predict([encoded, s0, c0])
-
+    prediction = modelx.predict([encoded, s0, c0])
+    
     predicted_text = []
     for i in range(len(prediction)):
         predicted_text.append(int(np.argmax(prediction[i], axis=1)))
-
+        
     predicted_text = list(predicted_text)
     predicted_text = int_to_string(predicted_text, inv_output_vocabulary)
     text_ = list(text)
-
+    
     # get the lengths of the string
     input_length = len(text)
     output_length = Ty
-
+    
     # Plot the attention_map
     plt.clf()
     f = plt.figure(figsize=(8, 8.5))
@@ -340,11 +395,10 @@ def plot_attention_map(model, input_vocabulary, inv_output_vocabulary, text, n_s
     # add grid and legend
     ax.grid()
 
-    # Save the figure instead of showing it
-    f.savefig('attention_map.png', bbox_inches='tight')
-
+    f.savefig("attention_map.png")
+    
     return attention_map
 
-text = "15 11 2005"
+text = "tuesday 09 october 1993" 
 # Call the function with the correct parameters
-plot_attention_map(model, human_vocab, inv_machine_vocab, text, n_s, Tx, Ty)
+plot_attention_map(model, human_vocab, inv_machine_vocab, text, n_s = 64, num = 7)
